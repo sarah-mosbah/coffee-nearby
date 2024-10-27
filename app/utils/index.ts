890a4827eeb06169287703d59de5529eed585744
sarah-mosbah <sarahmosbah101@gmail.com>
2024-10-27 20:41:51 +0300

    Update Domain

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,7 +1,7 @@
 export function getDomain(): URL {
   return new URL(
     process.env.NODE_ENV === "production"
-      ? "https://coffee-store.vercel.app"
+      ? "https://coffee-nearby-lqr4-eomq0tmdm-sarah-mosbahs-projects.vercel.app/"
       : "http://localhost:300"
   );
 }
